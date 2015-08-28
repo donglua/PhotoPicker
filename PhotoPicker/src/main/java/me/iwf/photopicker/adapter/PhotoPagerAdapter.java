a6bfcc5f93f29eb518c3bce1e6fc4becf4537c74
黄东鲁 <foolfalcon@hotmail.com>
2015-08-28 09:55:46 +0800

    修改 预览图片的布局

--- PhotoPicker/src/main/java/me/iwf/photopicker/adapter/PhotoPagerAdapter.java
+++ PhotoPicker/src/main/java/me/iwf/photopicker/adapter/PhotoPagerAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -46,7 +47,6 @@ public class PhotoPagerAdapter extends PagerAdapter {
     }
     Glide.with(mContext)
         .load(uri)
-        .override(800, 800)
         .placeholder(R.drawable.ic_photo_black_48dp)
         .error(R.drawable.ic_broken_image_black_48dp)
         .into(imageView);
