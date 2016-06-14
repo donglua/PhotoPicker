73016f0ec615ce67dceeb3e87ee38d233026da11
Matthew Zavislak <elevenfive@users.noreply.github.com>
2016-06-14 15:36:09 -0700

    Fix broken build
    - v7 appcompat requires v4 support
      https://developer.android.com/topic/libraries/support-library/features.html
    
    - PhotoPickerFragment needed a field added for listPopupWindow
    
    - Update Circle, Travis configs for new build tools
      - There are no tests, so no need to call connectedCheck
    
    - Update Android Gradle Plugin to 2.1.2

--- circle.sh
+++ circle.sh
@@ -10,7 +10,7 @@ function getAndroidSDK(){
     cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
     echo y | android update sdk -u -a -t android-23 &&
     echo y | android update sdk -u -a -t platform-tools &&
-    echo y | android update sdk -u -a -t build-tools-23.0.2 &&
+    echo y | android update sdk -u -a -t build-tools-23.0.3 &&
     touch $DEPS
   fi
 }
