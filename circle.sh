ffad4ee5a69fa10d2226d9be00f0a80a8ed1c5fc
黄东鲁 <foolfalcon@hotmail.com>
2016-09-22 20:47:49 +0800

    fix ci config

--- circle.sh
+++ circle.sh
@@ -8,9 +8,9 @@ function getAndroidSDK(){
 
   if [ ! -e $DEPS ]; then
     cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-    echo y | android update sdk -u -a -t android-23 &&
+    echo y | android update sdk -u -a -t android-24 &&
     echo y | android update sdk -u -a -t platform-tools &&
-    echo y | android update sdk -u -a -t build-tools-23.0.3 &&
+    echo y | android update sdk -u -a -t build-tools-24.0.2 &&
     touch $DEPS
   fi
 }
