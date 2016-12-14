5ce26e0cb1ff11e189160f422dd45549909be56e
yemengsky <yemengsky@gmail.com>
2016-12-14 19:49:00 +0800

    Add files via upload

---
+++ PhotoPicker/src/main/java/me/iwf/photopicker/utils/FileUtils.java
@@ -0,0 +1,28 @@
+package me.iwf.photopicker.utils;
+
+import android.util.Log;
+
+import java.io.File;
+
+/**
+ * Created by myc on 2016/12/14.
+ * More Code on 1101255053@qq.com
+ * Description:
+ */
+public class FileUtils {
+    public static boolean fileIsExists(String path) {
+        if (path == null || path.trim().length() <= 0) {
+            return false;
+        }
+        try {
+            File f = new File(path);
+            if (!f.exists()) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        Log.e("TMG",path+"file not exists");
+        return true;
+    }
+}
