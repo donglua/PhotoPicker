ad62082edf2a5a0a0fb9fc607de1f5a4712175bc
donglua <927878309@qq.com>
2017-06-07 20:38:46 +0800

    fix #164

---
+++ PhotoPicker/src/main/java/me/iwf/photopicker/utils/PhotoFileProvider.java
@@ -0,0 +1,10 @@
+package me.iwf.photopicker.utils;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by Donglu on 2017/6/7.
+ */
+
+public class PhotoFileProvider extends FileProvider {
+}
