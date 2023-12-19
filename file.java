30f0834787c2e80e94757a728f35d0d16d88b349
Arya Parashar <66545234+aryaparasharmrt@users.noreply.github.com>
2023-12-19 15:00:55 +0530

    Create file.java

---
+++ file.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Solution{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int val1 = sc.nextInt();
+    System.out.print("My value is: " + val1);
+}
+}
